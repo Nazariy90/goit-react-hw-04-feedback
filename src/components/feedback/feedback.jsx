0f9353b350c159ee790dd641b73de60580594fde
Nazariy90 <pinhvin123@gmail.com>
2023-05-04 20:11:45 +0300

    add

--- src/components/feedback/feedback.jsx
+++ src/components/feedback/feedback.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback, useMemo } from 'react';
 import { Statistics } from './Statistics';
 import { FeedbackOptions } from './FeedbackOptions';
 import { Section } from './Section';
@@ -7,37 +7,37 @@ import { Notification } from './Notification';
 export const FeedbackApp = () => {
   const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
 
-  const handleFeedback = option => {
+  const handleFeedback = useCallback(option => {
     setFeedback(prevState => ({
       ...prevState,
       [option]: prevState[option] + 1,
     }));
-  };
+  }, []);
 
-  const countTotalFeedback = () => {
+  const countTotalFeedback = useMemo(() => {
     const { good, neutral, bad } = feedback;
     return good + neutral + bad;
-  };
+  }, [feedback]);
 
-  const countPositiveFeedbackPercentage = () => {
+  const countPositiveFeedbackPercentage = useMemo(() => {
     const { good } = feedback;
-    const totalFeedback = countTotalFeedback();
+    const totalFeedback = countTotalFeedback;
     return totalFeedback === 0 ? 0 : Math.round((good / totalFeedback) * 100);
-  };
+  }, [feedback, countTotalFeedback]);
 
   return (
     <div>
       <Section title="Please leave feedback">
         <FeedbackOptions options={feedback} onLeaveFeedback={handleFeedback} />
       </Section>
-      {countTotalFeedback() > 0 ? (
+      {countTotalFeedback > 0 ? (
         <Section title="Statistics">
           <Statistics
             good={feedback.good}
             neutral={feedback.neutral}
             bad={feedback.bad}
-            total={countTotalFeedback()}
-            positivePercentage={countPositiveFeedbackPercentage()}
+            total={countTotalFeedback}
+            positivePercentage={countPositiveFeedbackPercentage}
           />
         </Section>
       ) : (
